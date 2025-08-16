7bff938c80bc532e0a9003472fb901ec47694649
alpha-x-team-ofc <alphaxteam21@gmail.com>
2025-08-16 20:39:10 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,6 +3,7 @@ const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "alive",
+    react: "☑️",
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
