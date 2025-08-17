f8040d8aad2112c817b433bc83f94fc13ec30b58
alpha-x-team-ofc <alphaxteam21@gmail.com>
2025-08-17 14:08:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "YOUR SESSION",
-  OWNER_NUM: process.env.OWNER_NUM || "94752978237 / 94770349867",
+  SESSION_ID: process.env.SESSION_ID || "",
+  OWNER_NUM: process.env.OWNER_NUM || "",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/dula9x/DARK-NOVA-XMD-V1-WEB-PAIR/blob/main/images/WhatsApp%20Image%202025-08-15%20at%2017.22.03_c520eb7b.jpg?raw=true",
   ALIVE_MSG: process.env.ALIVE_MSG || "Iam Alive Now!! ᴅᴀʀᴋ ɴᴏᴠᴀ xᴍᴅ 🤭💗 ආහ් පැටියෝ කොහොමද ?🌝!\n\n🥶ＭＡＤＥ ＢＹ ＡＬＰＨＡ Ｘ ＴＥＡＭ🥶",
@@ -15,7 +15,8 @@ module.exports = {
   MODE: process.env.MODE || "public",
   AUTO_VOICE: process.env.AUTO_VOICE || "true",
   AUTO_STICKER: process.env.AUTO_STICKER || "true",
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "",
+  MOVIE_API_KEY: process.env.MOVIE_API_KEY || "",
   AUTO_REPLY: process.env.AUTO_REPLY || "true",
-  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyA4NEiRrKlN6AuUGU_eaRzPjPUXAeo4ZrA",
-  MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|9d3c4942490b636ca58c82c6da4a599039358cdc",
+  
 };
