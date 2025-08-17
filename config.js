ea3a669aa745d16f8ae6fddb3454754fa8991b5e
alpha-x-team-ofc <alphaxteam21@gmail.com>
2025-08-17 09:17:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,4 +16,5 @@ module.exports = {
   AUTO_VOICE: process.env.AUTO_VOICE || "true",
   AUTO_STICKER: process.env.AUTO_STICKER || "true"
   AUTO_REPLY: process.env.AUTO_REPLY || "true"
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyA4NEiRrKlN6AuUGU_eaRzPjPUXAeo4ZrA",
 };
