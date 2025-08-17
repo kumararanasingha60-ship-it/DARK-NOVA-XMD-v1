cc6f5b8180d300946d88f0ab0d46e51696e2c7d5
alpha-x-team-ofc <alphaxteam21@gmail.com>
2025-08-17 08:54:37 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,8 @@ cmd({
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     await robin.sendPresenceUpdate('recording', from);
-    await robin.sendMessage(from, { audio: { url:"https://github.com/alpha-x-team-ofc/DARK-NOVA-XMD-v1/raw/refs/heads/main/audio/Welcome%20to...mp3"}, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+    await robin.sendMessage(from, { audio: { url : "https://github.com/alpha-x-team-ofc/DARK-NOVA-XMD-v1/raw/refs/heads/main/audio/Welcome%20to...mp3"}, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+    await robin.sendMessage(from,{sticker: { url : "https://github.com/alpha-x-team-ofc/DARK-NOVA-XMD-v1/raw/refs/heads/main/audio/%F0%9F%8E%AC/DN%20(1).webp"},package: 'DARK-NOVA-XMD'},{ quoted: mek })
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
